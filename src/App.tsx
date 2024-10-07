068a4a8c2afc20390079ddbd4fbc6021b9bf2997
EmhaHasyim <redeath001@gmail.com>
2024-10-07 12:32:07 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+const App = () => {
+
+  return (
+    <>
+      <section className='w-full flex justify-center items-center text-5xl gap-5 mt-10'>
+        AnimeChat Project
+      </section>
+    </>
+  )
+}
+
+export default App
